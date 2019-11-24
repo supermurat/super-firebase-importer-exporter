86bb116404b7e72ce6de950dd5390d43aa99ea82
Murat Demir <hisupermurat@gmail.com>
2019-11-24 22:34:42 +0300

    fix: downloading big data issue

--- src/download-data-from-firestore.ts
+++ src/download-data-from-firestore.ts
@@ -6,41 +6,49 @@ const dataFirestore = {};
 const downloadFromFirestore = async (): Promise<any> => {
     const mainCollectionSnapshot = await db.listCollections();
     for (const mainCollection of mainCollectionSnapshot) {
-
         if (config.download.collectionList.length > 0 &&
             config.download.collectionList.indexOf(mainCollection.id) === -1) {
             continue;
         }
+        const pageSize = 1000;
+        let currentPage = 0;
+        let mainDocsSnapshot;
         dataFirestore[mainCollection.id] = {};
-        const mainDocsSnapshot = await db.collection(mainCollection.id).get();
-        for (const mainDoc of mainDocsSnapshot.docs) {
-            dataFirestore[mainCollection.id][mainDoc.id] = mainDoc.data();
+        do {
+            mainDocsSnapshot = await db.collection(mainCollection.id)
+                .limit(pageSize)
+                .offset(currentPage * pageSize)
+                .get();
+            for (const mainDoc of mainDocsSnapshot.docs) {
+                dataFirestore[mainCollection.id][mainDoc.id] = mainDoc.data();
 
-            if (config.download.collectionListToCheckForSubCollections.length > 0 &&
-                config.download.collectionListToCheckForSubCollections.indexOf(mainCollection.id) === -1) {
-                console.log(mainCollection.id, mainDoc.id);
-                continue;
-            }
-            const subCollectionSnapshot = await db.collection(mainCollection.id)
-                .doc(mainDoc.id)
-                .listCollections();
-            for (const subCollection of subCollectionSnapshot) {
-                dataFirestore[mainCollection.id][mainDoc.id][`__collection__${subCollection.id}`] = {};
-
-                const subDocsSnapshot = await db.collection(mainCollection.id)
-                    .doc(mainDoc.id)
-                    .collection(subCollection.id)
-                    .get();
-                for (const subDoc of subDocsSnapshot.docs) {
-                    dataFirestore[mainCollection.id][mainDoc.id]
-                        [`__collection__${subCollection.id}`][subDoc.id] = subDoc.data();
-
-                    // console.log(mainCollection.id, mainDoc.id, subCollection.id, subDoc.id);
+                if (config.download.collectionListToCheckForSubCollections.length > 0 &&
+                    config.download.collectionListToCheckForSubCollections.indexOf(mainCollection.id) === -1) {
+                    console.log(mainCollection.id, mainDoc.id);
+                    continue;
                 }
-                console.log(mainCollection.id, mainDoc.id, subCollection.id);
+                const subCollectionSnapshot = await db.collection(mainCollection.id)
+                    .doc(mainDoc.id)
+                    .listCollections();
+                for (const subCollection of subCollectionSnapshot) {
+                    dataFirestore[mainCollection.id][mainDoc.id][`__collection__${subCollection.id}`] = {};
+
+                    const subDocsSnapshot = await db.collection(mainCollection.id)
+                        .doc(mainDoc.id)
+                        .collection(subCollection.id)
+                        .get();
+                    for (const subDoc of subDocsSnapshot.docs) {
+                        dataFirestore[mainCollection.id][mainDoc.id]
+                            [`__collection__${subCollection.id}`][subDoc.id] = subDoc.data();
+
+                        // console.log(mainCollection.id, mainDoc.id, subCollection.id, subDoc.id);
+                    }
+                    console.log(mainCollection.id, mainDoc.id, subCollection.id);
+                }
+                console.log(mainCollection.id, mainDoc.id);
             }
-            console.log(mainCollection.id, mainDoc.id);
-        }
+            currentPage++;
+        } while(mainDocsSnapshot.docs.length === pageSize);
         console.log(mainCollection.id);
     }
 };
